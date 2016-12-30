29b3e70474c8bb88672368a19f74d7036d685eba
Michael Chow <machow@princeton.edu>
2016-12-30 17:49:55 -0500

    Create requirements.sh

---
+++ requirements.sh
@@ -0,0 +1,7 @@
+pip3 install numpy==1.11.2
+pip3 install pandas==0.19.1
+pip3 install Jinja2==2.8
+pip3 install MarkupSafe==0.23
+pip3 install tornado==4.4.2
+pip3 install bokeh==0.12.3
+pip3 install requests==2.12.1
