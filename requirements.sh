2c64720d98c868d1eb03d0607681cb2b7a360754
Michael Chow <machow@princeton.edu>
2017-03-02 17:48:06 -0500

    Update requirements.sh

--- requirements.sh
+++ requirements.sh
@@ -1,1 +1,5 @@
-pip3 install git+https://github.com/datacamp/pythonwhat.git@feature-has-context
+pip3 install numpy==1.11.0
+pip3 install scipy==0.18.1
+pip3 install scikit-learn==0.18.1
+pip3 install keras==1.1.2
+pip3 install --upgrade https://storage.googleapis.com/tensorflow/linux/cpu/tensorflow-0.11.0-cp35-cp35m-linux_x86_64.whl
